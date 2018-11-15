3b56c8032936b3a0e9804cc3335e9a60c17c5739
Melvin <31832810+lakeey@users.noreply.github.com>
2018-11-14 20:17:07 -0600

    Delete nosuicide.lua

--- nosuicide.lua
+++
@@ -1,6 +0,0 @@
-local function BlockSuicide(ply)
-    ply:ChatPrint("It's not worth it, suicide is not the answer. Use @ and a staff member can cure your depression.")
-    return false
-end
-
-hook.Add( "CanPlayerSuicide", "BlockSuicide", BlockSuicide )
